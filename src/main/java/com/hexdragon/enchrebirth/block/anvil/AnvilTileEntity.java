b0615801b1e7d93841b1858dc72450f47379e3aa
LTCatt <1043137532@qq.com>
2020-11-13 20:20:23 +0800

    暂时取消了漏斗控制部分的代码

--- src/main/java/com/hexdragon/enchrebirth/block/anvil/AnvilTileEntity.java
+++ src/main/java/com/hexdragon/enchrebirth/block/anvil/AnvilTileEntity.java
@@ -13,18 +13,12 @@ import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SUpdateTileEntityPacket;
 import net.minecraft.tileentity.LockableLootTileEntity;
 import net.minecraft.tileentity.TileEntityType;
-import net.minecraft.util.Direction;
 import net.minecraft.util.IWorldPosCallable;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.Constants;
-import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.items.CapabilityItemHandler;
-import net.minecraftforge.items.ItemStackHandler;
 
-import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 // 铁砧 TileEntity，用于引导自定义模型与存储物品 NBT
@@ -97,7 +91,7 @@ public class AnvilTileEntity extends LockableLootTileEntity {
     }
 
     // TODO : [难顶] 让漏斗只能从上方注入物品（由于 Forge 接管了漏斗代码，需要使用它的 Capability，什么 SidedInventory 不管用），不过这个写起来还是不太对的样子
-    @Nonnull @Override public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+    /*@Nonnull @Override public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
         if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
             return LazyOptional.of(() -> new ItemStackHandler() {
                 @Override public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate) {
@@ -107,8 +101,9 @@ public class AnvilTileEntity extends LockableLootTileEntity {
             }).cast();
         }
         return super.getCapability(cap, side);
-    }
+    }*/
 
+    // TODO : 让目前的机制修改根据游戏难度略有变化
     // TODO : 由于目前的铁砧判断方式，它现在不接受线，因为线有对应的方块……
     // TODO : 让铁砧可以根据物品栏里的物品数量向比较器输出红石信号
 
