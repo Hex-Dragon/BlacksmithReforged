5f2b7c4dcf9d1608f055e8246ec720829a121fe7
LTCatt <1043137532@qq.com>
2020-11-17 19:34:03 +0800

    修改 TODO

--- src/main/java/com/hexdragon/enchrebirth/enchantment/DecayCurseEnchantment.java
+++ src/main/java/com/hexdragon/enchrebirth/enchantment/DecayCurseEnchantment.java
@@ -5,9 +5,9 @@ import net.minecraft.enchantment.EnchantmentType;
 import net.minecraft.inventory.EquipmentSlotType;
 
 // 腐朽诅咒：拥有这个附魔的工具在 修补、合并、从经验修补获得耐久度 时增加的耐久度下降 70%
-// TODO : 让腐朽诅咒可以作用于经验修补附魔，和物品合并的合成配方（Mixin 大概不够用了，这可能需要写一个新的 RepairItemRecipe）
+// TODO : 让腐朽诅咒可以作用于经验修补附魔，和物品合并的合成配方（Mixin 大概不够用了，这可能需要写一个新的 RepairItemRecipe；代码可以直接抄铁砧的）
 // TODO : 削弱经验修补附魔：每点经验值恢复 1 点耐久而不是 2 点
-// TODO : 修改耐久附魔：每次使用时消耗耐久度的概率将降至 30+(70/(0.5+等级))% （即耐久度每次都有 23%/42%/50% 的概率不会减少），这让盔甲上的耐久附魔变得更强，但让普通物品上的耐久附魔变得更弱了
+// TODO : 修改耐久附魔：每次使用时消耗耐久度的概率降至 100%/(1+0.5*等级) ，这让盔甲上的耐久附魔变得更强，但让普通物品上的耐久附魔变得更弱了
 public class DecayCurseEnchantment extends Enchantment {
     public DecayCurseEnchantment() {super(Rarity.VERY_RARE, EnchantmentType.BREAKABLE, new EquipmentSlotType[]{EquipmentSlotType.MAINHAND});}
 
