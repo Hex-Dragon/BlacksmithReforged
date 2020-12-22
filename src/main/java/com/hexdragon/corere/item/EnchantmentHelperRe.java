8e3fd8b8d6bac5a76715f5e83e4a2c4dc2ecda37
LTCatt <1043137532@qq.com>
2020-12-22 14:23:58 +0800

    调整数个编译相关的问题

--- src/main/java/com/hexdragon/corere/item/EnchantmentHelperRe.java
+++ src/main/java/com/hexdragon/corere/item/EnchantmentHelperRe.java
@@ -1,6 +1,5 @@
 package com.hexdragon.corere.item;
 
-import com.sun.istack.internal.NotNull;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.ItemStack;
@@ -11,12 +10,12 @@ import java.util.stream.Collectors;
 public class EnchantmentHelperRe {
 
     // 获取物品的所有诅咒附魔
-    public static Map<Enchantment, Integer> getCurseEnchantments(@NotNull ItemStack stack) {
+    public static Map<Enchantment, Integer> getCurseEnchantments(ItemStack stack) {
         return EnchantmentHelper.getEnchantments(stack).entrySet().stream().filter((pair) -> pair.getKey().isCurse()).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
     // 获取物品的所有非诅咒附魔
-    public static Map<Enchantment, Integer> getNonCurseEnchantments(@NotNull ItemStack stack) {
+    public static Map<Enchantment, Integer> getNonCurseEnchantments(ItemStack stack) {
         return EnchantmentHelper.getEnchantments(stack).entrySet().stream().filter((pair) -> !pair.getKey().isCurse()).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
