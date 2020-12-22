b3a6473774fd913dcbe93aa33cbbc8079c97770e
HerobrineXia <1727193915@qq.com>
2020-12-22 13:46:42 +0800

    Him测试Push

--- src/main/java/com/hexdragon/blacksmithre/enchantment/DecayCurseEnchantment.java
+++ src/main/java/com/hexdragon/blacksmithre/enchantment/DecayCurseEnchantment.java
@@ -5,6 +5,7 @@ import net.minecraft.enchantment.EnchantmentType;
 import net.minecraft.inventory.EquipmentSlotType;
 
 // 腐朽诅咒：拥有这个附魔的工具在 修补、合并、从经验修补获得耐久度 时增加的耐久度下降 70%
+// 测试Push
 public class DecayCurseEnchantment extends Enchantment {
     public DecayCurseEnchantment() {super(Rarity.VERY_RARE, EnchantmentType.BREAKABLE, new EquipmentSlotType[]{EquipmentSlotType.MAINHAND});}
 
